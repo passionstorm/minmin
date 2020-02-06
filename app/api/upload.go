36971c287a21fdfbdc6f749659e5dbe218bbae90
minhvh93 <vohoangminhdn93@gmail.com>
2020-02-06 07:13:53 +0700

    init

--- app/api/upload.go
+++ app/api/upload.go
@@ -3,44 +3,31 @@ package api
 import (
 	"github.com/kataras/iris/v12"
 	"io"
+	"minmin/app/constant"
+	"minmin/app/utils/app"
 	"os"
 	"strings"
 	"time"
 )
 
 func UploadAction(ctx iris.Context) {
-	defer coverErr(ctx)
+	defer app.CoverErr(ctx)
 	file, info, err := ctx.FormFile("file")
-	handlErr(err)
+	app.HandlErr(err)
 	defer file.Close()
 	now := time.Now()
 	fname := strings.Join([]string{
-		now.Format("20060102150405"),
+		now.Format(constant.YMDHms),
 		info.Filename,
 	}, "_")
 	out, err := os.OpenFile("./public/uploads/"+fname, os.O_WRONLY|os.O_CREATE, 0666)
-	handlErr(err)
+	app.HandlErr(err)
 	defer out.Close()
 	_, err = io.Copy(out, file)
-	handlErr(err)
+	app.HandlErr(err)
 	ctx.JSON(iris.Map{
 		"filename": fname,
 	})
 	return
 }
 
-func coverErr(ctx iris.Context) {
-	if r := recover(); r != nil {
-		ctx.StatusCode(400)
-		ctx.JSON(iris.Map{
-			"error": iris.Map{
-				"message": r,
-			},
-		})
-	}
-}
-func handlErr(err error) {
-	if err != nil {
-		panic(err.Error())
-	}
-}
---
+++ app/constant/date.go
@@ -0,0 +1,3 @@
+package constant
+
+const YMDHms = "20060102150405"
