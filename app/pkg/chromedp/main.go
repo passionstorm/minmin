fa9c0341622197ccdf2f969f8ec9d3145db809cc
minhvh <minhvh@soft-flight.work>
2020-03-10 16:41:38 +0700

    init

--- app/pkg/chromedp/main.go
+++ app/pkg/chromedp/main.go
@@ -15,7 +15,7 @@ import (
 const host = "https://www.facebook.com"
 
 func GetCookies() map[string]string {
-	str := `{"datr":"uugYXfeMFyXAvGdPt7C7PsWF","_fbp":"fb.1.1583028294666.428290832","locale":"vi_VN","m_pixel_ratio":"2","c_user":"100000027929102","xs":"26%3AgAO8EiO0Aa8H7g%3A2%3A1583562998%3A8037%3A6176","spin":"r.1001807630_b.trunk_t.1583677611_s.1_v.2_","fr":"14htpvUUenz3SnLaq.AWXrBnh38jvvbxNp6_UKCn8HzjA.BePiAo.dd.F5i.0.0.BeZQ1x.AWU0E4Gl","presence":"EDvF3EtimeF1583682318EuserFA21BB27929102A2EstateFDt3F_5bDiFA2user_3a1B04608994306A2ErF1EoF1EfF4C_5dElm3FA2user_3a1B04608994306A2Eutc3F1583682317580G583682318352CEchF_7bCC","act":"1583683392195%2F8","wd":"323x616"}`
+	str := `{"datr":"r7S_XZXAAhVOKS4rRnFe_cDA","c_user":"100000027929102","xs":"16%3AvrW_yOAMkLM-9A%3A2%3A1583136849%3A8037%3A6176","_fbp":"fb.1.1583399593262.720038549","spin":"r.1001807870_b.trunk_t.1583723572_s.1_v.2_","m_pixel_ratio":"1","x-referer":"eyJyIjoiL2hvbWUucGhwIiwiaCI6Ii9ob21lLnBocCIsInMiOiJtIn0%3D","wd":"1414x782","fr":"0JjvCGuFMFHambslt.AWVkxtC5mtc465fa_cimsl9DAbQ.Bdv6MD.VW.F5h.0.0.BeZu0G.AWXBCqfT","presence":"EDvF3EtimeF1583804325EuserFA21BB27929102A2EstateFDt3F_5bDiFA2user_3a1B04608994306A2EoF1EfF2C_5dEutc3F1583749830999G583749831362CEchF_7bCC","act":"1583804371104%2F2"}`
 	var rs map[string]string
 	err := json.Unmarshal([]byte(str), &rs)
 	if err != nil {
@@ -62,12 +62,35 @@ func main() {
 		chromedp.WithLogf(log.Printf),
 	)
 	defer cancel()
-	err := chromedp.Run(ctx, GetUrl("https://m.facebook.com/groups/"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	c, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
 
-	chromedp.Run(ctx, chromedp.Tasks{
+	var nodes []*cdp.Node
+	chromedp.Run(c, chromedp.Tasks{
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			// create cookie expiration
+			expr := cdp.TimeSinceEpoch(time.Now().Add(180 * 24 * time.Hour))
+			// add cookies to chrome
+			for k, v := range GetCookies() {
+				success, err := network.SetCookie(k, v).
+					WithExpires(&expr).
+					WithDomain(".facebook.com").
+					WithHTTPOnly(false).
+					Do(ctx)
+				if err != nil {
+					return err
+				}
+				if !success {
+					return fmt.Errorf("could not set cookie %q to %q", k, v)
+				}
+			}
+			return nil
+		}),
+		chromedp.Navigate("https://m.facebook.com/groups/"),
+		chromedp.ActionFunc(func(context.Context) error {
+			//log.Println("Access URL: " + url)
+			return nil
+		}),
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			_, exp, err := runtime.Evaluate(waitLoaded).Do(ctx)
 			if err != nil {
@@ -79,12 +102,12 @@ func main() {
 			log.Println("Page Ready!")
 			return nil
 		}),
-		chromedp.ActionFunc(func(context.Context) error {
-			var nodes []*cdp.Node
-			runtime.Evaluate(`document.querySelectorAll('a._7hkg');`)
+		chromedp.Nodes(`a._7hkg`, &nodes, chromedp.ByQueryAll),
+		chromedp.ActionFunc(func(ctx context.Context) error {
 			log.Println(nodes)
 			return nil
 		}),
+
 	})
 
 }
